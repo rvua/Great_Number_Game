f853a7dfac1f8c0f1979eec50cadf9f476e295fc
rvua <rvanore26@gmail.com>
2021-11-19 13:40:30 -0600

    bootstrap

--- server.py
+++ server.py
@@ -1,32 +1,26 @@
+from flask import Flask, render_template, session, redirect,request
 import random
-from flask import Flask, render_template, redirect, request, session
+
 app = Flask(__name__)
-app.secret_key = 'keepthissecret'
+
+app.secret_key="Benny bob wuz heer."
 
 @app.route('/')
 def index():
-    session['random_number'] = random.randint(1,100)
-    return render_template('index.html')
+    if "num" not in session:
+        session['num'] = random.randint(1,100)
 
-@app.route('/guess', methods=['POST'])
+    return render_template("index.html")
+
+@app.route('/guess',methods=['POST'])
 def guess():
     session['guess'] = int(request.form['guess'])
-    if 'guess' in session:
-        if session['guess'] == session['random_number']:
-            return redirect('/guessgame')
-        elif session['guess'] > session['random_number']:
-            return redirect('/guessgame')
-        else:
-            return redirect('/guessgame') 
-
-@app.route('/guessgame')
-def guessgame():
-    return render_template('guessgame.html')
-
-@app.route('/play_again')
-def play_again():
-    session.clear
     return redirect('/')
 
-if __name__ == "__main__":
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
     app.run(debug=True)
